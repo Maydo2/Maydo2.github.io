b43d2f1623fd152e548192adb3cac77e1dab8e94
Maydo20 <jac.raaijmakers@sintlucasedu.nl>
2024-10-01 14:18:33 +0200

    updating language display

---
+++ scripts/EnglishOrDutch.js
@@ -0,0 +1,12 @@
+let englishText = false;
+
+function changeLanguage(language){
+    if (language = "english"){
+        englishText = true;
+    }
+    else{
+        englishText = false;
+    }
+
+    console.log(englishText);
+}
