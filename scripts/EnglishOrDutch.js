39c22a3e98cea7ea0a2c6b9d58369971b273f548
Maydo20 <jac.raaijmakers@sintlucasedu.nl>
2024-10-01 17:09:57 +0200

    about me page working with js

--- scripts/EnglishOrDutch.js
+++ scripts/EnglishOrDutch.js
@@ -1,11 +1,29 @@
-let englishText = false;
-
 function changeLanguage(language){
     if (language === "english"){
-        englishText = true;
+        localStorage.removeItem("language")
+        localStorage.setItem("language", "en");
     }
     else if(language === "dutch"){
-        englishText = false;
+        localStorage.removeItem("language")
+        localStorage.setItem("language", "nl");
     }
-    console.log(englishText);
+}
+
+function setAboutMeText() {
+    const aboutMeContent = document.getElementById("content");
+
+    console.log(localStorage.getItem("language"));
+
+    if (localStorage.getItem("language") === "en"){
+        aboutMeContent.innerHTML = `<p class="topText">About Me</p>
+            Welcome to my portfolio page. <br> 
+            My name is Jacques Raaijmakers, I'm studying as a software developer. 
+            I'm currently in my second year at Sintlucas Eindhoven.`;
+    }
+    else{
+        aboutMeContent.innerHTML = `<p class="topText">Over mij</p>
+            Welkom op mijn portfolio site. <br>
+            Mijn naam is Jacques Raaijmakers, ik ben in opleiding als software developer.
+            Ik zit momenteel in mijn tweede jaar op het Sintlucas Eindhoven.`;
+    }
 }
